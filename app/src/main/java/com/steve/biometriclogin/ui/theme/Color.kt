9a8d9363e497e8c33740501723cdb9aa0ece2b8f
stevechacha <stevechacha4@gmail.com>
2021-10-13 10:32:09 +0300

    Initial commit

---
+++ app/src/main/java/com/steve/biometriclogin/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.steve.biometriclogin.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+
+
+val LightGray100 = Color(0xFFe2e2e2)
